00393e54890396e1f73d3ad064d59e4daf747532
rajat2207 <66623568+rajat2207@users.noreply.github.com>
2020-07-31 02:12:25 +0530

    Add Combine Reducers Method

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,10 @@
-import {ADD_MOVIES, ADD_TO_FAVOURITES, REMOVE_FROM_FAVOURITES, SET_SHOW_FAVOURITES} from '../actions/index';
+import {combineReducers} from 'redux';
+import {
+    ADD_MOVIES,
+    ADD_TO_FAVOURITES,
+    REMOVE_FROM_FAVOURITES,
+    SET_SHOW_FAVOURITES
+} from '../actions/index';
 
 const initialMoviesState = {
     list: [],
@@ -61,9 +67,14 @@ const initialRootState = {
     search: initialSearchState
 }
 
-export default function rootReducer (state = initialRootState,action){
-    return {
-        movies: movies(state.movies,action),
-        search: search(state.search,action)
-    }
-}
+// export default function rootReducer (state = initialRootState,action){
+//     return {
+//         movies: movies(state.movies,action),
+//         search: search(state.search,action)
+//     }
+// }
+
+export default combineReducers({
+    movies,
+    search
+})
