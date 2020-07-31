b66b0a736550674caafc7fb2d89da718b7e44a29
rajat2207 <66623568+rajat2207@users.noreply.github.com>
2020-07-31 13:10:12 +0530

    Add Search UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,59 @@
 import React from 'react';
+import { data } from '../data';
 
 class Navbar extends React.Component {
 
-    render () {
-        return (
-            <div className="nav">
-              <div className="search-container">
-                <input/>
-                <button id="search-btn">Search</button>
+  constructor(props){
+    super(props);
+    this.state = {
+      showSearchResults: true,
+      searchText:''
+    }
+  }
+
+  handleAddToMovies = () => {
+
+  } 
+
+  handleChange = (event) => {
+    this.setState({
+      searchText : event.target.value 
+    })
+  }
+
+  // handleSearch = () => {
+  //   const {searchText} = this.state;
+  //   const {store} = this.props;
+
+  //   store.dispatch(handleMovieSearch(searchText));
+  // }
+
+  render () {
+
+    const {showSearchResults} =this.state;
+    return (
+        <div className="nav">
+          <div className="search-container">
+            <input onChange={this.handleChange} />
+            <button id="search-btn" onClick={this.handleSearch}>Search</button>
+
+            {showSearchResults && (
+            <div className="search-results">
+              <div className="search-result">
+                <img src={data[0].Poster} alt="search-pic" />
+                <div className="movie-info">
+                  <span>{data[0].Title}</span>
+                  <button onClick={this.handleAddToMovies}>
+                    Add to Movies
+                  </button>
+                </div>
               </div>
             </div>
-          );
-    }
+          )}
+          </div>
+        </div>
+    );
+  }
 }
 
 export default Navbar;
